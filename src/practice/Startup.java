0b06d4df1316cc1afd055634b27ef1c07bc3f5dd
mjmersenski <mjmersenski@192.168.1.2>
2017-11-12 12:56:48 -0600

    created an integer arraylist and did console output for ascending and descending order

--- src/practice/Startup.java
+++ src/practice/Startup.java
@@ -1,9 +1,39 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author mjmersenski
  */
 public class Startup {
+    public static void main(String[] args) {
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(1);
+        numberList.add(2);
+        numberList.add(4);
+        numberList.add(3);
+        
+        // not sorted
+        for(int x : numberList){
+            System.out.println(x);
+        }
+        
+        // sorted in numeric order
+        Collections.sort(numberList);
+        System.out.println("---------------");
+        for(int x : numberList){
+            System.out.println(x);
+        }
+        
+        // sorted in reverse numeric order
+        Collections.sort(numberList, Collections.reverseOrder());
+        System.out.println("---------------");
+        for(int x : numberList){
+            System.out.println(x);
+        }
+    }            
     
 }
